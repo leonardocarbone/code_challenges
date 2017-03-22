6b48cb3589a429698427ed038b517977472033ef
Leonardo Carbone <leonardoz.carbone@gmail.com>
2017-03-22 10:35:23 -0300

    Update unpaired.py

--- unpaired.py
+++ unpaired.py
@@ -1,16 +1,21 @@
 # Find unpaired value that occurs in odd number of elements.
 
 # Solution 1
-lst = [9, 3, 9, 3, 9, 7, 9]
-
 def find_unpaired(list):
-  while len(list) >= 0:
-    if not list[0] in list[1:]:
-      return list[0]
-      
+  list.sort()
   
-    list = filter(lambda x: x!= list[0], list)  
-    
+  index = 0
+  while index < len(list)-1:
+    if list[index] != list[index+1]:
+      return list[index]
+      
+    index+=2
+  
+  return list[index]
 
 
-print find_unpaired(lst)
+print find_unpaired([3, 3, 9, 9, 10])
+print find_unpaired([5, 3, 5])
+print find_unpaired([42])
+print find_unpaired([5, 2, 3, 1, 2, 5, 3])
+print find_unpaired([5, 2, 3, 4, 4, 5, 3])
